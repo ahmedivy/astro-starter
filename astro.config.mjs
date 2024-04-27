89c0ec5bcd45caf3f062f1cb2fee15f345875eea
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-04-27 17:11:15 +0500

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    starlight({
+      title: "My delightful docs site",
+    }),
+    // mdx(),
+    sitemap(),
+  ],
 });
---
+++ src/content/docs/index.md
@@ -0,0 +1,8 @@
+---
+title: My docs
+description: Learn more about my project in this docs site built with Starlight.
+---
+
+Welcome to my project!
+
+> This is a next level project by a next level developer in a next level framework.
